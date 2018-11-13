8671052c77056013713caeeeb675a51c490075d1
Gustavo Muniz do Carmo <gustavo@codeyourinfra.today>
2018-11-13 16:11:36 +0000

    travis ci build trigger action

--- actions.py
+++ actions.py
@@ -37,3 +37,28 @@ class SlackWebhookPost():
                                                                         current_info["latest_release_number"])
         message = {"text": message_text}
         requests.post(self.__slack_webhook_url, data=message)
+
+class TravisCIBuildTrigger():
+    """
+    Class responsible for triggering a build in Travis CI.
+    """
+
+
+    def __init__(self, travis_api_token, repo_api_endpoint):
+        self.__travis_api_token = travis_api_token
+        self.__repo_api_endpoint = repo_api_endpoint
+
+
+    def execute(self, platform, library_name, latest_info, current_info):
+        build_message = "New *%s* version released in *%s*: *%s* !!!" % (library_name,
+                                                                         platform,
+                                                                         current_info["latest_release_number"])
+        body = {"request": {
+            "message": build_message,
+            "branch": "master"
+        }}
+        headers = {
+            "Travis-API-Version": "3",
+            "Authorization": "token %s" % self.__travis_api_token
+        }
+        requests.post(self.__repo_api_endpoint, data=body, headers=headers)
