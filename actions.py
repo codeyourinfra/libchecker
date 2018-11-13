b1cb9bd6dee2e958a7ee037951e93d1039a87fb5
Gustavo Muniz do Carmo <gustavo@codeyourinfra.today>
2018-11-13 15:12:04 +0000

    slack webhook post action

--- actions.py
+++ actions.py
@@ -19,3 +19,21 @@ class WebhookPost():
                    "latest_info": latest_info,
                    "current_info": current_info}
         requests.post(self.__webhook_url, data=payload)
+
+
+class SlackWebhookPost():
+    """
+    Class responsible for posting a message to a Slack incoming webhook.
+    """
+
+
+    def __init__(self, slack_webhook_url):
+        self.__slack_webhook_url = slack_webhook_url
+
+
+    def execute(self, platform, library_name, latest_info, current_info):
+        message_text = "New *%s* version released in *%s*: *%s* !!!" % (library_name,
+                                                                        platform,
+                                                                        current_info["latest_release_number"])
+        message = {"text": message_text}
+        requests.post(self.__slack_webhook_url, data=message)
